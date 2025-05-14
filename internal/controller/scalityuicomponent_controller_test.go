03af812833c2df9ebeb35f66a0d8e48ec15ec7cc
Hervé Dombya <135591453+hervedombya@users.noreply.github.com>
2025-05-14 13:45:59 +0200

    Revert "Add Service creation and reconciliation to ScalityUIComponent controller"
    This reverts commit 166b7b2a8018070c3e3f5cd3f95aa1b03e378af3.

--- internal/controller/scalityuicomponent_controller_test.go
+++ internal/controller/scalityuicomponent_controller_test.go
@@ -25,8 +25,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	uiv1alpha1 "github.com/scality/ui-operator/api/v1alpha1"
@@ -79,29 +77,8 @@ var _ = Describe("ScalityUIComponent Controller", func() {
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
-
-			By("Checking if Deployment was created with correct specifications")
-			deployment := &appsv1.Deployment{}
-			err = k8sClient.Get(ctx, typeNamespacedName, deployment)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(deployment.Spec.Template.Spec.Containers).To(HaveLen(1))
-			Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(Equal(testImage))
-			Expect(deployment.Spec.Template.Spec.Containers[0].Name).To(Equal(resourceName))
-
-			Expect(deployment.Spec.Template.ObjectMeta.Labels["app"]).To(Equal(resourceName))
-			Expect(deployment.Spec.Selector.MatchLabels["app"]).To(Equal(resourceName))
-
-			By("Checking if Service was created with correct specifications")
-			service := &corev1.Service{}
-			err = k8sClient.Get(ctx, typeNamespacedName, service)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(service.Spec.Selector["app"]).To(Equal(resourceName))
-			Expect(service.Spec.Ports).To(HaveLen(1))
-			Expect(service.Spec.Ports[0].Name).To(Equal("http"))
-			Expect(service.Spec.Ports[0].Protocol).To(Equal(corev1.ProtocolTCP))
-			Expect(service.Spec.Ports[0].Port).To(Equal(int32(80)))
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
 	})
 })
