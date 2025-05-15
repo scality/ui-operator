803e06840aa2dcf4854c4e93597bf21debefe0fa
Hervé Dombya <135591453+hervedombya@users.noreply.github.com>
2025-05-15 14:46:38 +0200

    Fix mountPath bug
    Add Service creation and reconciliation to ScalityUIComponent controller
    
    Revert "Add Service creation and reconciliation to ScalityUIComponent controller"
    
    This reverts commit 166b7b2a8018070c3e3f5cd3f95aa1b03e378af3.

--- internal/controller/scalityuicomponent_controller.go
+++ internal/controller/scalityuicomponent_controller.go
@@ -19,9 +19,6 @@ package controller
 import (
 	"context"
 
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -127,7 +124,5 @@ func (r *ScalityUIComponentReconciler) Reconcile(ctx context.Context, req ctrl.R
 func (r *ScalityUIComponentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&uiv1alpha1.ScalityUIComponent{}).
-		Owns(&appsv1.Deployment{}).
-		Owns(&corev1.Service{}).
 		Complete(r)
 }
